5455eef460e1d724e2ee2ab214b5c34f5238f625
Kirtwinder <rhys13@live.com>
2021-05-18 00:38:10 -0700

    Updates

---
+++ precache-manifest.a573be7a20531a4f35fe4109de7ca86a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d4880c967c539a08c90",
+    "url": "/static/js/main.1d4880c9.chunk.js"
+  },
+  {
+    "revision": "6a4c0b2a812b4677363c",
+    "url": "/static/js/1.6a4c0b2a.chunk.js"
+  },
+  {
+    "revision": "5bca8a00930b43c93513",
+    "url": "/static/js/2.5bca8a00.chunk.js"
+  },
+  {
+    "revision": "8e7a25b31d8519c80dee",
+    "url": "/static/js/3.8e7a25b3.chunk.js"
+  },
+  {
+    "revision": "41562f14018e7cae43e6",
+    "url": "/static/js/4.41562f14.chunk.js"
+  },
+  {
+    "revision": "c18688021dfd6e0a8c50",
+    "url": "/static/js/runtime~main.c1868802.js"
+  },
+  {
+    "revision": "94a746255b0a840fba164bf1aee9a814",
+    "url": "/index.html"
+  }
+];
